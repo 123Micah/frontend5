98df24bb5564bcb1055547651a3d40563fb87826
Micah Okperuvwe <luckyokperuvwe@gmail.com>
2025-08-08 12:21:07 +0100

    s

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
-  // baseURL: 'https://backend5-2jm8.onrender.com/api',
+    //baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://backend5-2jm8.onrender.com/api',
 });
 
 // Attach token automatically
